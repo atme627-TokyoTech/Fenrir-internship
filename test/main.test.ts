2347ff07c4828906dbc48a91921ec141381d6889
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-06-19 00:56:48 +0900

    add testcases

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,13 @@
 import { isValid } from '../src/main';
 
-test('test', () => {
+test('test1', () => {
     expect(isValid('()')).toBe(true);
+});
+
+test('test2', () => {
+    expect(isValid('([]){}')).toBe(true);
+});
+
+test('test3', () => {
+    expect(isValid('{)}')).toBe(false);
 });
